dca00b2bf9ea83cd68b4a7ab3f1b2fe96271f816
ShadowsJerker <shadowsjerker@gmail.com>
2021-08-15 01:37:34 +0430

    Working on the click function

--- indexScriptAndCss/indexScript.js
+++ indexScriptAndCss/indexScript.js
@@ -1,5 +1,12 @@
 var openCheck = false;
 
+var details = 
+  [
+    "This text is a test for cutting details of properties. This text is going to be previewed and when the item get a \"onclick\" event it's going to expand",
+    "This text is a test for cutting details of properties. This text is going to be previewed and when the item get a \"onclick\" event it's going to expand",
+    "This text is a test for cutting details of properties. This text is going to be previewed and when the item get a \"onclick\" event it's going to expand"
+  ]
+
 function pageHeaderClick(a)
 {
     var el = document.getElementById(a)
@@ -21,3 +28,26 @@ function pageHeaderClick(a)
 
     
 }
+
+
+var detailsSave = details;
+  function pageStartUp()
+  {
+    for(var j = 0; 0 < details.length; j++)
+  {
+    var stringForDes = "";
+    
+    for(var i = 80; i < 105; i++)
+    {
+      if(details[j].substring(0, i)[i-1] == ' ')
+      {
+        stringForDes = details[j].substring(0, i);
+        console.log(document.getElementById('card1DescrptionId' + (j + 1)).innerHTML)
+        document.getElementById('card1DescrptionId' + (j + 1)).innerHTML = stringForDes + "...";
+        break;
+      }
+    }
+  }
+  details = detailsSave;
+  }
+  pageStartUp();
--- slider/sliderScript.js
+++ slider/sliderScript.js
@@ -59,6 +59,10 @@ function titleClicked(itemId)
         document.getElementById(itemId).style.left = "50%";
         document.getElementById(itemId).style.transform = "translate(-50%,-50%)";
         document.getElementById(itemId).style.zIndex = "99";
+        for(var j = 0; 0 < details.length; j++)
+        {
+            document.getElementById('card1DescrptionId' + (j + 1)).innerHTML = details[j];
+        }
     }
     else if(document.getElementById(itemId).style.position == "absolute")
     {
@@ -68,9 +72,46 @@ function titleClicked(itemId)
         document.getElementById(itemId).style.top =  "";
         document.getElementById(itemId).style.left = "";
         document.getElementById(itemId).style.transform = "";
-        document.getElementById(itemId).style.zIndex = "0";
+        (itemId).style.zIndex = "0";
+   
+        console.log("From slider script: " + details[1]);
+        for(var j = 0; 0 < details.length; j++)
+        {
+          var stringForDes = "";
+          
+          for(var i = 80; i < 105; i++)
+          {
+            if(details[j].substring(0, i)[i-1] == ' ')
+            {
+              stringForDes = details[j].substring(0, i);
+              console.log(document.getElementById('card1DescrptionId' + (j + 1)).innerHTML)
+              document.getElementById('card1DescrptionId' + (j + 1)).innerHTML = stringForDes + "...";
+              break;
+            }
+          }
+        }
+        
     }
-
+    console.log("From slider script: " + details[1]);
+    if(details[0] != document.getElementById('card1DescrptionId').innerHTML)
+    {
+        for(var j = 0; 0 < details.length; j++)
+        {
+          var stringForDes = "";
+          
+          for(var i = 80; i < 105; i++)
+          {
+            if(details[j].substring(0, i)[i-1] == ' ')
+            {
+              stringForDes = details[j].substring(0, i);
+              console.log(document.getElementById('card1DescrptionId' + (j + 1)).innerHTML)
+              document.getElementById('card1DescrptionId' + (j + 1)).innerHTML = stringForDes + "...";
+              break;
+            }
+          }
+        }
+    }
+    
      
 
      //position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);
