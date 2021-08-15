770121f8167d3303b3c378317b7d7570c9a36f9e
ShadowsJerker <shadowsjerker@gmail.com>
2021-08-15 23:20:46 +0430

    item updated
    Fixed the item description cutting and expanding

--- slider/sliderScript.js
+++ slider/sliderScript.js
@@ -66,53 +66,24 @@ function titleClicked(itemId)
     }
     else if(document.getElementById(itemId).style.position == "absolute")
     {
+        itemClose(itemId);  
+        pageStartUp(); 
+    }
+     //position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);
+}
+
+function itemClose(itemId)
+{
         document.getElementById(itemId).style.position = "";
         document.getElementById(itemId).style.width = "80%";
         document.getElementById(itemId).style.height = "";
         document.getElementById(itemId).style.top =  "";
         document.getElementById(itemId).style.left = "";
         document.getElementById(itemId).style.transform = "";
-        (itemId).style.zIndex = "0";
-   
-        console.log("From slider script: " + details[1]);
-        for(var j = 0; 0 < details.length; j++)
-        {
-          var stringForDes = "";
-          
-          for(var i = 80; i < 105; i++)
-          {
-            if(details[j].substring(0, i)[i-1] == ' ')
-            {
-              stringForDes = details[j].substring(0, i);
-              console.log(document.getElementById('card1DescrptionId' + (j + 1)).innerHTML)
-              document.getElementById('card1DescrptionId' + (j + 1)).innerHTML = stringForDes + "...";
-              break;
-            }
-          }
-        }
-        
-    }
-    console.log("From slider script: " + details[1]);
-    if(details[0] != document.getElementById('card1DescrptionId').innerHTML)
-    {
-        for(var j = 0; 0 < details.length; j++)
-        {
-          var stringForDes = "";
-          
-          for(var i = 80; i < 105; i++)
-          {
-            if(details[j].substring(0, i)[i-1] == ' ')
-            {
-              stringForDes = details[j].substring(0, i);
-              console.log(document.getElementById('card1DescrptionId' + (j + 1)).innerHTML)
-              document.getElementById('card1DescrptionId' + (j + 1)).innerHTML = stringForDes + "...";
-              break;
-            }
-          }
-        }
-    }
-    
-     
+        document.getElementById(itemId).style.zIndex = "0";
 
-     //position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);
+        setTimeout(1000);
+        pageStartUp();
+        
+        console.log("From slider script: " + details[1]);
 }
