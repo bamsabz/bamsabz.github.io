0c743f86ea5ffc0ebd8ed082665bdd3f76fced8a
ShadowsJerker <shadowsjerker@gmail.com>
2021-09-03 18:56:45 +0430

    s

--- cssAndJs/contactUs.js
+++ cssAndJs/contactUs.js
@@ -1,8 +1,9 @@
-function formSubmitClick()
+function submitFormClick()
 {
     console.log("submited");
 
     var _phoneNumber = document.getElementById("phoneNumber").value;
+    console.log(_phoneNumber);
     _phoneNumber += "@gmail.com";
     document.getElementById("phoneNumber").value = _phoneNumber;
 }
